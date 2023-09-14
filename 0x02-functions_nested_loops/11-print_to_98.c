0670ac4c6440ac26e02de947bce9cfd8f2a2d3f4
peterea71 <peteredozie71@gmail.com>
2023-09-14 10:42:04 -0700

    alx-low_level_programming

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
-* print_to_98 - Prints all natural numbers from input to 98,
-*		in order separated by a comma followed by a space.
-* @n: The number input.
-* Return: Always 0 (Success)
-*/
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *		in order separated by a comma followed by a space.
+ * @n: The number input.
+ * Return: Always 0 (Success)
+ */
 void print_to_98(int n)
 {
 	if (n >= 98)
